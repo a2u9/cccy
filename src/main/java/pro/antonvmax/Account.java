c0e500816a30da7e189897a782b47632a3f81479
a2u9 <a2u9dev@gmail.com>
2018-08-04 17:05:38 +0500

    + Account

---
+++ src/main/java/pro/antonvmax/Account.java
@@ -0,0 +1,17 @@
+package pro.antonvmax;
+
+public class Account {
+    private int balance;
+
+    public Account(int balance) {
+        this.balance = balance;
+    }
+
+    public void withdraw(int amount) {
+        balance -= amount;
+    }
+
+    public void deposit(int amount) {
+        balance += amount;
+    }
+}
